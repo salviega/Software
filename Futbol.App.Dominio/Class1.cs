bf6fabd77550de02352006c209e6c919919501ba
julianc97 <juliancaviedes97@gmail.com>
2021-10-04 09:30:38 -0500

    prueba final

--- Futbol.App.Dominio/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Futbol.App.Dominio
-{
-    public class Class1
-    {
-    }
-}
--- Futbol.App.Persistencia/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Futbol.App.Persistencia
-{
-    public class Class1
-    {
-    }
-}
